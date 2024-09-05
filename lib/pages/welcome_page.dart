cc0158c7cc7be96bbb3b13594e017d67000a9f90
Krevetka17 <163000963+Krevetka17@users.noreply.github.com>
2024-09-05 11:41:36 +0200

    Added widgets for app_text

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
 
 class WelcomePage extends StatefulWidget {
   const WelcomePage({super.key});
@@ -28,7 +29,20 @@ class _WelcomePageState extends State<WelcomePage> {
               image: DecorationImage(
                   image: AssetImage(
                    "img/"+images[index]
+                  ),
+                fit: BoxFit.cover
+              )
+            ),
+            child: Container(
+              margin: const EdgeInsets.only(top:150, left: 20, right: 20),
+              child:Row(
+                children: [
+                  Column(
+                    children: [
+                      AppLargeText(text: "Trips"),
+                    ],
                   )
+                ],
               )
             ),
           );
