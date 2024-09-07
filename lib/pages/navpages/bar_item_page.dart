259e37af6f1d7e106d17f492fb74764175157e0b
Krevetka17 <163000963+Krevetka17@users.noreply.github.com>
2024-09-07 19:31:57 +0200

    Start building nav pages

---
+++ lib/pages/navpages/bar_item_page.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_cubit/pages/navpages/my_page.dart';
+
+class BarItemPage extends StatelessWidget {
+  const BarItemPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+          child: Text(
+              "Bar Item Page"
+          ),
+        )
+    );
+  }
+}
---
+++ lib/pages/navpages/main_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.apps)),
+          BottomNavigationBarItem(label: "Bar", icon: Icon(Icons.bar_chart_sharp)),
+          BottomNavigationBarItem(label: "Search", icon: Icon(Icons.search)),
+          BottomNavigationBarItem(label: "My", icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
