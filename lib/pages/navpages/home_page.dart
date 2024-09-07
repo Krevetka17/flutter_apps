e69225713ba2e1f9c094f363b3c97619e1746b95
Krevetka17 <163000963+Krevetka17@users.noreply.github.com>
2024-09-07 20:55:25 +0200

    BottomNavigationbar

--- lib/pages/navpages/home_page.dart
+++ lib/pages/navpages/home_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_cubit/widgets/app_large_text.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -7,9 +9,67 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    TabController _tabController = TabController(length: 3, vsync: this);
+    return Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          //menu text
+          Container(
+            padding: const EdgeInsets.only(top: 70, left: 20),
+            child: Row(
+              children: [
+                Icon(Icons.menu, size: 30, color: Colors.black54),
+                Expanded(child: Container()),
+                Container(
+                  margin: const EdgeInsets.only(right: 20),
+                  width: 50,
+                  height: 50,
+
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey.withOpacity(0.5),
+                  ),
+                )
+              ],
+            ),
+          ),
+          SizedBox(height: 40),
+          //discover text
+          Container(
+            margin: const EdgeInsets.only(left: 20),
+            child: AppLargeText(text: "Discover"),
+          ),
+          SizedBox(height: 30),
+          //tabbar
+          Container(
+            child: TabBar(
+              controller: _tabController,
+
+              tabs: [
+                Tab(text: "Places"),
+                Tab(text: "Inspiration"),
+                Tab(text: "Emotions"),
+              ],
+            ),
+          ),
+          Container(
+            height: 300,
+            width: double.maxFinite,
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                Text("Hi"),
+                Text("There"),
+                Text("Bye"),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/pages/navpages/main_page.dart
+++ lib/pages/navpages/main_page.dart
@@ -19,11 +19,29 @@ class _MainPageState extends State<MainPage> {
     SearchPage(),
     MyPage()
   ];
+  int currentIndex=0;
+  void onTap( int index){
+    setState(() {
+      currentIndex = index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: pages[0] ,
+      backgroundColor: Colors.white,
+      body: pages[currentIndex] ,
       bottomNavigationBar: BottomNavigationBar(
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: Colors.white,
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Colors.black54,
+        unselectedItemColor: Colors.grey.withOpacity(0.5),
+        showUnselectedLabels: false,
+        showSelectedLabels: false,
+        elevation: 0,
         items: [
           BottomNavigationBarItem(label: "Home", icon: Icon(Icons.apps)),
           BottomNavigationBarItem(label: "Bar", icon: Icon(Icons.bar_chart_sharp)),
